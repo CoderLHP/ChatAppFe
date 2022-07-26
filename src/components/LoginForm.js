ef2b1d5dcd69e9ae4187ea87bd7257fdd2ba28f6
Nghia Hoang <nghiax32@gmail.com>
2022-07-26 16:23:57 +0700

    Nghia_220726_4

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,14 @@
 import React, {useState} from 'react'
 
+function showPassword() {
+  var password = document.getElementById('password');
+  if(password.type === 'password') {
+    password.type = 'text';
+  } else {
+    password.type = 'password';
+  }
+}
+
 function LoginForm({Login, error}) {
   const [details, setDetails] = useState({username: "", password: ""});
 
@@ -14,13 +23,15 @@ function LoginForm({Login, error}) {
         <div className='form-inner'>
             <h2>Login</h2>
             <div className='form-group'>
-                {/* <label htmlFor='username'>Username:</label> */}
                 <input type='text' name='username' id='username' placeholder='Username' onChange={e => setDetails({...details, username: e.target.value})} value={details.username}/>
             </div>
             <div className='form-group'>
-              {/* <label htmlFor='password'>Password:</label> */}
               <input type='password' name='password' id='password' placeholder='Password' onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
             </div>
+            <div className='form-group'>
+              <input type='checkbox' name='showPassword' id='checkbox' onClick={e => showPassword()}/>Show Password
+            </div>
+            
             {(error != "") ? (<div className='error'>{error}</div>) : ""}
             <input type= 'submit' value = 'Log In'/>
         </div>
